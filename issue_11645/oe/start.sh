88c6e6d1a831e6fc4bd92056a6754f8cd99178ba
Illya Havsiyevych <illya.havsiyevych@gmail.com>
2020-04-29 08:24:55 +0300

    no --no-ancient-blocks for validator

--- issue_11645/oe/start.sh
+++ issue_11645/oe/start.sh
@@ -4,10 +4,9 @@ OE=/home/adria/bin/oe-3.0.0-alpha
 #OE=/home/adria/bin/parity-2.7.2-stable 
 #OE=/home/adria/bin/parity-2.5.13-stable 
 
-BOOTNODES="enode://14b3f9ff18928230ec35bc0938ee8a0f6d29cfa1172e385995a47db6528f9e62d2ea46b2bdd112f720d0b35cb4aba741cede4aa1ae19c17fa82727ecbce8053c@127.0.0.1:30303,enode://9de648d5d7f60c6e0d2ceb6785323dffeeaeb03b9bd18219ac803d424d466718fc56ee1d9984c320e3d1ee019f2fd0733ab603c73fa2b8996f9207a2991b636c@127.0.0.l:30304,enode://3b8e57ca809a61049abe2a97219eb931039eb98be98fe4a61bc849e2f20b571d72d13af03e1a6647b711b1e03d721256e926d814e3e90d707a4054a5608fd08b@127.0.0.1:30305"
+BOOTNODES="enode://14b3f9ff18928230ec35bc0938ee8a0f6d29cfa1172e385995a47db6528f9e62d2ea46b2bdd112f720d0b35cb4aba741cede4aa1ae19c17fa82727ecbce8053c@127.0.0.1:30303,enode://9de648d5d7f60c6e0d2ceb6785323dffeeaeb03b9bd18219ac803d424d466718fc56ee1d9984c320e3d1ee019f2fd0733ab603c73fa2b8996f9207a2991b636c@127.0.0.1:30304,enode://3b8e57ca809a61049abe2a97219eb931039eb98be98fe4a61bc849e2f20b571d72d13af03e1a6647b711b1e03d721256e926d814e3e90d707a4054a5608fd08b@127.0.0.1:30305"
 CONFIG="--chain=chain.json --config=authority.toml --password ./password.txt --bootnodes $BOOTNODES"  
-EXTRA="--no-ancient-blocks"  
-PARAMS="$CONFIG $EXTRA" 
+PARAMS="$CONFIG" 
 
 rm -rf node1/cache node1/chains
 rm -rf node2/cache node2/chains
