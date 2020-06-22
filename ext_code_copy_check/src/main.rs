5a2c5dc637aa675aa8dde5110a34ef3d6e70908b
adria0 <adria@codecontext.io>
2020-06-22 17:00:45 +0200

    +info

--- ext_code_copy_check/src/main.rs
+++ ext_code_copy_check/src/main.rs
@@ -5,9 +5,6 @@ use web3::futures::Future;
 use web3::types::*;
 
 /*
-pragma solidity ^0.6.0;
-
-contract ext_code_copy_check {
     function check(address[] calldata _addrs) external view returns (uint256) {
         uint256 o_code;
         assembly {
@@ -22,7 +19,21 @@ contract ext_code_copy_check {
         }
         return start_gas-gasleft();
     }
-}
+    event Run();
+    function run(address[] calldata _addrs) external {
+        uint256 o_code;
+        assembly {
+            o_code := mload(0x40)
+        }
+        uint256 start_gas = gasleft();
+        for (uint256 i =0 ; i<_addrs.length; ++i) {
+            address _addr = _addrs[i];
+            assembly {
+                extcodecopy(_addr, o_code, 0, 1)
+            }
+        }
+        emit Run();
+    }
 */
 const ABI: &str = r#"
 [
@@ -68,6 +79,33 @@ fn do_call<T : web3::Transport >(contract: &EasyContract<T>, addrs : Vec<Address
 
     println!("gas used in call with {} contracts: {} {}s", len, gas_used.as_u64(), start.elapsed().as_secs_f64());
 }
+
+/*
+./openethereum --chain=dev --jsonrpc-interface=all --unsafe-expose --jsonrpc-cors=all --tx-gas-limit=20000000
+fn do_tx(addrs : Vec<Address>) {
+    let web3 = new_web3();
+    let len = addrs.len();
+    let parity_dev  = Account::from_secret_key("4d5db4107d237df6a3d58ee5f70ae63d73d7658d4026f2eefd2f204c81682cb7");
+
+    let evmcode = (include_str!("../code_sol_ext_code_size_check.bin"))
+            .from_hex::<Vec<u8>>()
+            .expect("cannot parse evmcode");
+
+    let abi_json = include_bytes!("../code_sol_ext_code_size_check.abi");
+    let address = EasyContract::deploy(&web3, &parity_dev, evmcode,U256::zero())
+            .expect("cannot deploy contract");
+
+    let contract = EasyContract::from_json(&web3, address, abi_json)
+          .expect("cannot assign to contract");
+
+    let start = std::time::Instant::now();
+    let recipt = contract
+        .call("check", addrs, &parity_dev, U256::zero())
+        .expect("cannot tx check");
+
+    println!("gas used in tx with {} contracts: {} {}s", len, recipt.gas_used.unwrap().as_u64(), start.elapsed().as_secs_f64());
+}
+*/
 
 fn scrap_contracts() {
     let web3 = new_web3();
