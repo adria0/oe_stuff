c0a4213b839128bd25ee03d5b82e1d9603ab4f73
adria0 <adria@codecontext.io>
2020-07-15 12:25:47 +0200

    fix

--- find_damm_commit/run.sh
+++ find_damm_commit/run.sh
@@ -13,7 +13,7 @@ if [ "$#" -lt 1 ]; then
 fi
 COMMIT=$1
 OEARG=$2
-BOOTNODES=--bootnodes enode://68f46370191198b71a1595dd453c489bbfe28036a9951fc0397fabd1b77462930b3c5a5359b20e99677855939be47b39fc8edcf1e9ff2522a922b86d233bf2df@144.217.153.76:30303,enode://ffed6382e05ee42854d862f08e4e39b8452c50a5a5d399072c40f9a0b2d4ad34b0eb5312455ad8bcf0dcb4ce969dc89a9a9fd00183eaf8abf46bbcc59dc6e9d5@51.195.3.238:30303
+BOOTNODES="--bootnodes enode://68f46370191198b71a1595dd453c489bbfe28036a9951fc0397fabd1b77462930b3c5a5359b20e99677855939be47b39fc8edcf1e9ff2522a922b86d233bf2df@144.217.153.76:30303,enode://ffed6382e05ee42854d862f08e4e39b8452c50a5a5d399072c40f9a0b2d4ad34b0eb5312455ad8bcf0dcb4ce969dc89a9a9fd00183eaf8abf46bbcc59dc6e9d5@51.195.3.238:30303"
 ( cd ../oe_monitor && cargo build --release )
 git clone http://github.com/openethereum/openethereum
 ( cd openethereum && git checkout $1 )
